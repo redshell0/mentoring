43023e3ecbbcd67daae43472137c5da85bba9c2e
redshell0 <baseball0020@gmail.com>
2020-09-25 15:21:47 +0900

    update

--- src/components/Xss.js
+++ src/components/Xss.js
@@ -80,6 +80,64 @@ const Xss = () => {
           전송하게 된다.
         </p>
       </div>
+      <h1 style={{ marginTop: 100 }}># Stored XSS</h1>
+      <div className="wrapper">
+        <p>
+          Stored XSS는 Reflected XSS와는 달리 웹 사이트의 게시판에 스크립트
+          삽입하는 공격 방식이다.
+        </p>
+
+        <p>
+          공격자는 게시판에 스크립트를 삽입한 후 공격 대상자가 해당 게시글을
+          클릭하도록 유도한다.
+        </p>
+        <img
+          src="https://t1.daumcdn.net/cfile/tistory/22662A4B58C4E00705"
+          alt="10"
+          style={{ backgroundColor: "#fff" }}
+        />
+        <p>
+          공격자가 미리 XSS 공격에 취약한 웹사이트를 탐색하고 XSS 공격을 위한
+          스크립트를 포함 한 게시글을 웹 사이트에 업로드 한다.
+        </p>
+
+        <p>
+          게시글의 URL을 사용자에게 노출하고 사용자가 게시글을 확인함으로써
+          URL에 대한 요청을 서버에 전송하게 된다.
+        </p>
+
+        <p>
+          웹 서버에서 스크립트를 포함한 Response를 전송하며 공격이 수행된다.
+        </p>
+      </div>
+      <h1 style={{ marginTop: 100 }}># XSS 방지법</h1>
+      <div className="wrapper">
+        <p>
+          XSS 공격은 IPS, IDS, 방화벽 등으로도 방지할 수가 없다. 때문에 단순히
+          문자를 필터링 하는 등의 방법만이 존재한다.
+        </p>
+        <h3 style={{ marginTop: 30 }}>1. script 문자 필터링</h3>
+        <p>
+          XSS 공격은 입력값에 대한 검증이 제대로 이루어지지 않아 발생하는
+          취약점이다.
+        </p>
+        <p>
+          때문에 사용자의 모든 입력값에 대하여 서버측에서 필터링을 해 주어야
+          한다.
+        </p>
+        <p>
+          PHP의 eregi 함수를 이용하면 XSS 공격에 주로 사용되는 문자들을 필터링
+          할 수 있다.
+        </p>
+        <p>
+          주로 스크립트를 실행하기 위한 특수문자를 필터링 하며 ", ' 등의 문자가
+          있다.
+        </p>
+        <p>JavaScript로는 replaceAll 함수가 있다.</p>
+        <h3 style={{ marginTop: 30 }}>1. script 문자 필터링</h3>
+        <p>PHP 함수 중 htmlentities라는 함수가 있다.</p>
+        <p>htmlentities는 모든 특수문자를 HTML 엔티티(entity)로 변환한다.</p>
+      </div>
     </motion.div>
   );
 };
